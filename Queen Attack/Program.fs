3d9291f2a928a9b5605f71ff002157acfd60a832
Damian Green <damian@gcoders.com>
2021-07-25 22:38:34 +0100

    Queen attack simplification

--- Queen Attack/Program.fs
+++ Queen Attack/Program.fs
@@ -1,27 +1,31 @@
 open System
+let length = 8
 
 [<EntryPoint>]
 let main argv =
     let random = Random()
 
-    let makeRandomPos () = (random.Next(0, 8), random.Next(0, 8))
+    let makeRandomPos () =
+        (random.Next(1, length + 1), random.Next(1, length + 1))
 
-    let (wx, wy) = makeRandomPos ()
-    let (bx, by) = makeRandomPos ()
+    let whitePos = makeRandomPos ()
+    let blackPos = makeRandomPos ()
+    let (wx, wy) = whitePos
+    let (bx, by) = blackPos
 
     printfn "WHITE: %d,%d" wx wy
     printfn "BLACK: %d,%d" bx by
     printfn "Matched horizontal or vertical: %b" ((wx = bx) || (wy = by))
     printfn "Matched diagonal: %b" (abs (bx - wx) = abs (by - wy))
 
-    let posMatchesWhite (x, y) = x = wx && y = wy
-    let posMatchesBlack (x, y) = x = bx && y = by
+    let posMatchesWhite pos = pos = whitePos
+    let posMatchesBlack pos = pos = blackPos
 
-    for i in 0 .. 7 do
+    for y in 1 .. length do
         printf "\n"
 
-        for j in 0 .. 7 do
-            let pos = (i, j)
+        for x in 1 .. length do
+            let pos = (x, y)
 
             match pos with
             | pos when posMatchesWhite pos -> "W"
