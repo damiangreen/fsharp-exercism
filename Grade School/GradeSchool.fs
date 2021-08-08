b8b3bc8c022b6e713ed87aece4c49fbf8dbb3ca3
Damian Green <damian@gcoders.com>
2021-08-08 11:23:53 +0100

    Grade School

---
+++ Grade School/GradeSchool.fs
@@ -0,0 +1,22 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let grade (grade: int) (school: School) =
+    match school.TryGetValue grade with
+    | true, value -> value |> List.sort
+    | _ -> []
+
+let add (student: string) (gradeV: int) (school: School) : School =
+    school.Add(gradeV, grade gradeV school @ [ student ]) // inefficent?
+
+let roster (school: School) : string list =
+    let students =
+        school
+        |> Map.toSeq
+        |> (Seq.map (fun (_, v) -> List.sort v))
+        |> List.concat
+
+    students
